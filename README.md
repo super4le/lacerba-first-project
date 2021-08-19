0b7e25af9e8c6d64872ad9aab5a31c6c38238cce
super4le <alessandracursio@hotmail.it>
2021-08-19 11:49:25 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# My First Node Project
+
+## This is the first node project for Lacerba
+
+To run the project, use `ts-node main.ts`
---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello World!");
+console.log("This is my first Node Project");
+var myVar = 3;
+myVar += 10;
+console.log('myVar = ', myVar);
